89f44dc4a27cce948adb53c51927b7b8d79d0166
Adlibaari <93728649+Adlibaari@users.noreply.github.com>
2023-03-26 14:23:29 +0700

    Update Tugas1.py

--- Tugas1.py
+++ Tugas1.py
@@ -28,7 +28,7 @@ with col1:
       Y1 = st.selectbox(
       'Y',
       ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'), key='Chart2')
-      fig = px.scatter(data_frame=Data,x=X1, y=Y1,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area, hover_name="Country", hover_data=['  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'])
+      fig = px.scatter(data_frame=Data,x=X1, y=Y1,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
       st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 with col2:
@@ -38,7 +38,7 @@ with col2:
       Y2 = st.selectbox(
       'Y',
       ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'),  key='Chart4')
-      fig = px.scatter(data_frame=Data,x=X2, y=Y2,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area, hover_name="Country", hover_data=['  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'])
+      fig = px.scatter(data_frame=Data,x=X2, y=Y2,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
       st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 with col3:
@@ -48,7 +48,7 @@ with col3:
       Y3 = st.selectbox(
       'Y',
       ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'),  key='Chart6')
-      fig = px.scatter(data_frame=Data,x=X3, y=Y3,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area, hover_name="Country", hover_data=['  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'])
+      fig = px.scatter(data_frame=Data,x=X3, y=Y3,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
       st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 with col4:
@@ -58,7 +58,7 @@ with col4:
       Y4 = st.selectbox(
       'Y',
       ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'), key='Chart8')
-      fig = px.scatter(data_frame=Data,x=X4, y=Y4,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area, hover_name="Country", hover_data=['  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'])
+      fig = px.scatter(data_frame=Data,x=X4, y=Y4,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
       st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 
