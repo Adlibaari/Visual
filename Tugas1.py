5fff81d0515e7735885193d87da61927ff2d3933
Adlibaari <93728649+Adlibaari@users.noreply.github.com>
2023-04-13 21:58:57 +0700

    Update Tugas1.py

--- Tugas1.py
+++ Tugas1.py
@@ -6,18 +6,20 @@ url = 'https://raw.githubusercontent.com/Adlibaari/Visual/main/factbook.csv'
 Data = pd.read_csv(url)
 
 col1,col2 = st.columns(2)
-
+col3, col4 = st.columns(2)
 
 with col1:
       X1 = st.selectbox(
       'X',
       (Data.columns.values), key='Chart1')
-      Y1 = st.selectbox(
-      'Y',
-      (Data.columns.values), key='Chart2')
       Size = st.selectbox(
       'Size',
       (Data.columns.values), key='Chart5')
+ 
+with col2:
+      Y1 = st.selectbox(
+      'Y',
+      (Data.columns.values), key='Chart2')
       Color = st.selectbox(
       'Color',
       (Data.columns.values), key='Chart6')
@@ -26,21 +28,24 @@ Circle_area = st.slider('Circle Area', 0, 100, 60)
 fig = px.scatter(data_frame=Data,x=X1, y=Y1,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
 st.plotly_chart(fig, use_container_width=True)
 
-with col2:
+with col3:
       X2 = st.selectbox(
       'X',
       (Data.columns.values), key='Chart3')
-      Y2 = st.selectbox(
-      'Y',
-      (Data.columns.values),  key='Chart4')
       Size = st.selectbox(
       'Size',
       (Data.columns.values), key='Chart7')
+
+with col3:
+      Y2 = st.selectbox(
+      'Y',
+      (Data.columns.values),  key='Chart4')
       Color = st.selectbox(
       'Color',
       (Data.columns.values), key='Chart8')
-      Circle_area = st.slider('Circle Area', 0, 100, 60)
-      fig = px.scatter(data_frame=Data,x=X2, y=Y2,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
-      st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+      
+Circle_area = st.slider('Circle Area', 0, 100, 60)
+fig = px.scatter(data_frame=Data,x=X2, y=Y2,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 
